dd52b40a70b510077422d3d324522bf8b7eee224
akimasa-l <75195951+I-2ki@users.noreply.github.com>
2022-08-23 12:30:53 +0900

    Initial Commit

---
+++ morokoshi_seiko_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  morokoshi_seiko_app
+//
+//  Created by jimbo hajime on 2022/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ morokoshi_seiko_app/morokoshi_seiko_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  morokoshi_seiko_appApp.swift
+//  morokoshi_seiko_app
+//
+//  Created by jimbo hajime on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct morokoshi_seiko_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
